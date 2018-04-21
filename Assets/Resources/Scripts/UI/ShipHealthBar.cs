345196a7147e377368b6505096401a165b7c66d0
piotrlipert <piotr.lipert@gmail.com>
2018-04-21 19:53:07 +0200

    Some new assets + FunctionalBars

--- Assets/Resources/Scripts/UI/ShipHealthBar.cs
+++ Assets/Resources/Scripts/UI/ShipHealthBar.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipHealthBar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    SpriteMask sm;
+    SpriteRenderer filler;
+    SpriteRenderer background;
+
+    public float percentFilled = 0f;
+
+
+	void Start ()
+    {
+        sm = transform.Find("FillerSpriteMask").GetComponent<SpriteMask>();
+        filler = transform.Find("Filler").GetComponent<SpriteRenderer>();
+        sm.sprite = filler.sprite;
+        background = GetComponent<SpriteRenderer>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	void Update ()
+    {
+
+        setSpriteMaskSizeAndPosition();
+        	
 	}
+
+    public void setSpriteMaskSizeAndPosition()
+    {
+        float width = filler.sprite.bounds.size.x;
+
+        sm.transform.localScale = new Vector3(1f - percentFilled, 1f, 1f);
+        float offset = (width * percentFilled)/2f;
+        sm.transform.localPosition = new Vector3(offset, sm.transform.localPosition.y, sm.transform.localPosition.z);
+    }
 }
