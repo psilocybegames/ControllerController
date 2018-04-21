68601fca9620a982a69c350b00542e0fad62decf
Soga-ki <sebastiano.pciau@gmail.com>
2018-04-21 22:36:13 +0200

    Added Ladder prefab
    Added a Ladder prefab and completed the ladder script
    The ladder script now takes an integer number of tiles and procedurally generates a ladder of that size.

--- Assets/Resources/Scripts/Passages/Ladder.cs
+++ Assets/Resources/Scripts/Passages/Ladder.cs
@@ -6,24 +6,55 @@ public class Ladder : MonoBehaviour {
 
     private PilotController pilot;
 
+    public uint tileHeight;
+
+    GameObject[] tiles;
+
 	// Use this for initialization
 	void Start ()
     {
+        float nextTileYPos = 0;
+        Vector2 colliderSize;
+
         pilot = FindObjectOfType<PilotController>();
-	}
-	
-	// Update is called once per frame
-	void OnTriggerEnter2d (Collider2D other)
+        GameObject ladderTile = Resources.Load("Materials/LadderTile", typeof(GameObject)) as GameObject;
+        tiles = new GameObject[tileHeight];
+        
+        //generate all the ladder tiles needed
+        for (uint i = 0; i < tileHeight; i++)
+        {
+            tiles[i] = GameObject.Instantiate(ladderTile) as GameObject;
+            tiles[i].transform.parent = gameObject.transform;
+            Vector3 tilePosition = new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + nextTileYPos), gameObject.transform.position.z);
+            tiles[i].transform.SetPositionAndRotation(tilePosition, gameObject.transform.rotation);
+            nextTileYPos += tiles[i].GetComponent<SpriteRenderer>().size.y;
+        }
+        //remove the height of one tile (that was not added because we exited the cycle)
+        nextTileYPos -= tiles[0].GetComponent<SpriteRenderer>().size.y;
+        // make collider width 1/3 of the actual width of the ladder to reduce the "floating on ladder" effect
+        float colliderWidth = tiles[0].GetComponent<SpriteRenderer>().size.x / 3;
+        
+        colliderSize = new Vector2(colliderWidth, nextTileYPos);
+        //create the collider
+        BoxCollider2D ladderCollider = gameObject.AddComponent<BoxCollider2D>();
+        ladderCollider.size = colliderSize;
+        ladderCollider.isTrigger = true;
+        //offset the collider to center it vertically
+        ladderCollider.offset = new Vector2(0, (nextTileYPos / 2));
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D other)
     {
-		if(other == pilot)
+		if(other.name == pilot.name)
         {
             pilot.SetIsOnLadder(true);
         }
 	}
 
-    void OnTriggerExit2d(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
-        if (other == pilot)
+        if (other.name == pilot.name)
         {
             pilot.SetIsOnLadder(false);
         }
