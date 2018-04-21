42f1ec84cbc41fd92a21493d1e8c18c50c9c5ead
Soga-ki <sebastiano.pciau@gmail.com>
2018-04-21 20:27:44 +0200

    Added Ladder behaviour
    Informed player controller when entering/leaving a ladder.
    Disabled gravity while on ladder

--- Assets/Resources/Scripts/Passages/Ladder.cs
+++ Assets/Resources/Scripts/Passages/Ladder.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour {
 
+    private PilotController pilot;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        pilot = FindObjectOfType<PilotController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter2d (Collider2D other)
+    {
+		if(other == pilot)
+        {
+            pilot.IsOnLadder = true;
+        }
 	}
+
+    void OnTriggerExit2d(Collider2D other)
+    {
+        if (other == pilot)
+        {
+            pilot.IsOnLadder = false;
+        }
+    }
 }
