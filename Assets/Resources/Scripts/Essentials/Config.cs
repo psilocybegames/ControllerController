eae3a2bc34675ea6b702abf1a00235b5b6594e66
piotrlipert <piotr.lipert@gmail.com>
2018-04-21 18:42:04 +0200

    Added cursor with stick input axes

---
+++ Assets/Resources/Scripts/Essentials/Config.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Config
+{
+    public static float CursorSpeed = 0.1f;
+}
---
+++ Assets/Resources/Scripts/ThePilot/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    public float horDir = 0f;
+    public float verDir = 0f;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        horDir = Input.GetAxis("Cursor X");
+        verDir = Input.GetAxis("Cursor Y");
+
+    }
+
+    void FixedUpdate()
+    {
+
+        transform.position += new Vector3(horDir, verDir, 0f) * Config.CursorSpeed;
+
+
+    }
+}
