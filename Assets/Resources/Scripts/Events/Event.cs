b5f8329149f54114ab104a5113ddc92078756e4a
piotrlipert <piotr.lipert@gmail.com>
2018-04-22 15:55:25 +0200

    Events and component prototypes

---
+++ Assets/Resources/Scripts/Events/Event.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event {
+
+
+    public float startTime = 0f;
+    public float endTime = 0f;
+    public float duration = 15f;
+    public float severity = 1f;
+
+
+
+    public virtual void processEvent()
+    {
+
+
+
+
+    }
+
+    public virtual void initEvent(float sTime)
+    {
+
+        startTime = sTime;
+        endTime = sTime + duration;
+
+    }
+
+    public virtual void onFireEvent()
+    {
+
+
+
+    }
+
+
+    public virtual void onEndEvent()
+    {
+
+
+
+
+    }
+
+
+
+      
+}
+
+
